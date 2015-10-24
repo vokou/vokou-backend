5eb7c76e18de1a62935571d61232b7d606f0c691
Zhuosheng Jiang <jemit.uriel@gmail.com>
2015-10-23 19:35:52 -0700

    upgrade install

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+sudo apt-get update
 sudo apt-get install coffeescript
 sudo apt-get install libfontconfig1
 sudo apt-get install libjpeg8
 sudo apt-get install nodejs-legacy
 sudo apt-get install npm
-sudo apt-get update
 sudo npm install forever -g
