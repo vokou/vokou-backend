d368bfcd31125a633e5087c0ab041eb9c0cdfff6
Zhuosheng Jiang <jemit.uriel@gmail.com>
2015-11-03 08:33:07 -0800

    aa

--- hyatt.js
+++ hyatt.js
@@ -46,7 +46,8 @@ else{
             tempObject.lsp = 9999;
           }else{
             tempObject.lsp = tempObject.price/ratio;
-            tempObject.passportPoint = Number(tempObject.passportPoint.replace(/'/g,''));
+            tempObject.passportPoint = tempObject.passportPoint.replace(/,/g,'');
+            tempObject.passportPoint = Number(tempObject.passportPoint);
             tempObject.pp = {point_plan: "Points", value: tempObject.lsp/tempObject.passportPoint};
             tempObject.points = tempObject.passportPoint;
           }
